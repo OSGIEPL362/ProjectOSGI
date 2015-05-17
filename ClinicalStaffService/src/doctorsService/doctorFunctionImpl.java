add876f037b8e68a4b453ee7487613d796dcc400
pparthenhs <pparthenhs@gmail.com>
2015-05-18 02:36:25 +0300

    Revert "Medical done"
    This reverts commit 4262e6dbe2db1cdd741c435cb0c699e67f52eada.

--- ClinicalStaffService/src/doctorsService/doctorFunctionImpl.java
+++ ClinicalStaffService/src/doctorsService/doctorFunctionImpl.java
@@ -18,345 +18,330 @@ public class doctorFunctionImpl implements doctorFunctions {
 	@Override
 	public ResultSet getRandevous(int doctor_id) {
 		// TODO Auto-generated method stub
-		try {
+		try{
 			connectDB connection = new connectDB();
 			String query = "SELECT * FROM `randevou`, `patients` WHERE randevou.Patient_ID = patients.Patient_ID and randevou.Doctor_ID= "
-					+ doctor_id
-					+ " and  randevou.Date = CURDATE() and patients.Dead=0;";
+						+doctor_id+" and  randevou.Date = CURDATE() and patients.Dead=0;";
 			connection.resSet = connection.stmt.executeQuery(query);
 			return connection.resSet;
-
-		} catch (SQLException se) {
-			se.printStackTrace();
+			
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
-
+		};
+		
 		return null;
 	}
 
 	@Override
 	public ResultSet getRandevouInfo(int randevou_id) {
 		// TODO Auto-generated method stub
-		try {
+		try{
 			connectDB connection = new connectDB();
-			String query = "SELECT * FROM `randevou`, `patients` WHERE randevou.Patient_ID = patients.Patient_ID and randevou.Randevou_ID = "
-					+ randevou_id + " ;";
+			String query = "SELECT * FROM `randevou`, `patients` WHERE randevou.Patient_ID = patients.Patient_ID and randevou.Randevou_ID = "+randevou_id+" ;";
 			connection.resSet = connection.stmt.executeQuery(query);
 			return connection.resSet;
-		} catch (SQLException se) {
-			se.printStackTrace();
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return null;
 	}
 
 	@Override
 	public String[] getLastConditionAndMedicationOfPatient(int patient_id) {
 		// TODO Auto-generated method stub
-
-		try {
+	
+		try{
 			connectDB connection = new connectDB();
-			String query = "SELECT * FROM `randevou` WHERE Patient_ID = "
-					+ patient_id
-					+ " and date IN (SELECT max(date) FROM `randevou` WHERE Date < CURDATE() and Append = 1 and Updated = 1 and Patient_ID = "
-					+ patient_id + ")";
-			// String query =
-			// "SELECT * FROM `randevou` WHERE date IN (SELECT max(date) FROM `randevou` WHERE Date < CURDATE() and Append = 1 and Updated = 1 and Patient_ID = "+patient_id+")";
-			// SELECT * FROM `randevou` WHERE Patient_ID = "+patient_id+" and
-			// date IN (SELECT max(date) FROM `randevou` WHERE Date < CURDATE()
-			// and Append = 1 and Updated = 1 and Patient_ID = "+patient_id+")";
-			connection.resSet = connection.stmt.executeQuery(query);
-			// return connection.resSet;
-
-			String[] a = new String[3];
-			if (connection.resSet.next()) {
+			String query = "SELECT * FROM `randevou` WHERE Patient_ID = "+patient_id+" and date IN (SELECT max(date) FROM `randevou` WHERE Date < CURDATE() and Append = 1 and Updated = 1 and Patient_ID = "+patient_id+")";
+			//String query = "SELECT * FROM `randevou` WHERE date IN (SELECT max(date) FROM `randevou` WHERE Date < CURDATE() and Append = 1 and Updated = 1 and Patient_ID = "+patient_id+")";
+			//SELECT * FROM `randevou` WHERE Patient_ID = "+patient_id+" and date IN (SELECT max(date) FROM `randevou` WHERE Date < CURDATE() and Append = 1 and Updated = 1 and Patient_ID = "+patient_id+")";
+			connection.resSet = connection.stmt.executeQuery(query);			
+			//return connection.resSet;
+			
+			String [] a = new String[3];
+			if(connection.resSet.next()){
 				a[0] = connection.resSet.getString("Conditions");
 				a[1] = connection.resSet.getString("Medication_ID");
-				a[2] = TakeAllCommendsFromPesient(patient_id);
+				a[2]=TakeAllCommendsFromPesient(patient_id);
 			}
 			return a;
-		} catch (SQLException se) {
-			se.printStackTrace();
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return null;
 	}
 
-	public String TakeAllCommendsFromPesient(int patient_id) {
-		String s = "";
-		try {
-			connectDB connection = new connectDB();
-			String query = "SELECT * FROM `randevou` WHERE Updated=1 and Append=1 and Date<CURDATE() and Patient_ID="
-					+ patient_id + "";
-			connection.resSet = connection.stmt.executeQuery(query);
-			while (connection.resSet.next()) {
-				s = s + "---------------" + connection.resSet.getString("Date")
-						+ "--------" + "\n";
-				s = s + connection.resSet.getString("Comments") + "\n";
-			}
-			return s + "\n";
-		} catch (SQLException se) {
-			se.printStackTrace();
+	
+	public String TakeAllCommendsFromPesient(int patient_id){
+		String s="";
+		try{
+		connectDB connection = new connectDB();
+		String query = "SELECT * FROM `randevou` WHERE Updated=1 and Append=1 and Date<CURDATE() and Patient_ID="+patient_id+"";
+		connection.resSet = connection.stmt.executeQuery(query);	
+		while(connection.resSet.next()){
+			s=s+"---------------"+connection.resSet.getString("Date")+"--------"+"\n";
+			s=s+connection.resSet.getString("Comments")+"\n";
+		}
+		return s+"\n";
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return null;
 	}
-
+	
 	@Override
 	public ResultSet getDrugs() {
 		// TODO Auto-generated method stub
-		try {
+		try{
 			connectDB connection = new connectDB();
 			String query = "SELECT * FROM `drugs` ;";
 			connection.resSet = connection.stmt.executeQuery(query);
 			return connection.resSet;
-		} catch (SQLException se) {
-			se.printStackTrace();
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return null;
 	}
 
 	@Override
-	public boolean updateRandevou(int randevou_id, String Condition,
-			int Medication, String comments, int overule, int lm, int dead,
-			int sh, int patient_id) {
+	public boolean updateRandevou(int randevou_id, String Condition, int Medication, String comments,
+			int overule, int lm, int dead, int sh, int patient_id) {
 		// TODO Auto-generated method stub
 		try {
 			connectDB connection = new connectDB();
-			String query1 = "UPDATE randevou SET  Medication_ID = "
-					+ Medication + ", Conditions = '" + Condition
-					+ "', Comments = '" + comments + "', Overule = " + overule
-					+ " , Updated = " + 1 + " WHERE Randevou_ID = "
-					+ randevou_id + ";";
-			System.out.println(query1);
-			connection.stmt.executeUpdate(query1);
-
-			String query2 = "UPDATE patients SET Madness_level = " + lm
-					+ ", Dead = " + dead + ", Self_harm = " + sh
-					+ " WHERE Patient_ID=" + patient_id;
-			connection.stmt.executeUpdate(query2);
-
+			String query1= "UPDATE randevou SET  Medication_ID = "+Medication+", Conditions = '"+Condition+"', Comments = '"+comments+
+					"', Overule = "+overule+ " , Updated = "+1+" WHERE Randevou_ID = "+randevou_id+";";
+					System.out.println(query1);
+			connection.stmt.executeUpdate(query1); 
+			
+			String query2 = "UPDATE patients SET Madness_level = "+lm+", Dead = "+dead+", Self_harm = "+sh+" WHERE Patient_ID="+patient_id;
+			connection.stmt.executeUpdate(query2); 
+			
+		
 			return true;
-		} catch (SQLException se) {
-			// Handle errors for JDBC
-			se.printStackTrace();
+		}catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return false;
-
+		
 	}
 
 	@Override
 	public ResultSet getNotUpdatedRandevous(int doctor_id) {
 		// TODO Auto-generated method stub
-		try {
+		try{
 			connectDB connection = new connectDB();
-			String query = "SELECT * FROM `randevou`, `patients` WHERE randevou.Patient_ID = patients.Patient_ID and randevou.Append=1 and randevou.Updated = 0 and randevou.Doctor_ID= "
-					+ doctor_id
-					+ " and CURDATE() > randevou.date and patients.Dead=0;";
+			String query = "SELECT * FROM `randevou`, `patients` WHERE randevou.Patient_ID = patients.Patient_ID and randevou.Append=1 and randevou.Updated = 0 and randevou.Doctor_ID= "+doctor_id+" and CURDATE() > randevou.date and patients.Dead=0;";
 			connection.resSet = connection.stmt.executeQuery(query);
 			System.out.println(query);
 			return connection.resSet;
-		} catch (SQLException se) {
-			se.printStackTrace();
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return null;
 	}
 
 	@Override
-	public boolean insertIncentend(int patient_id, String details,
-			int medication) {
+	public boolean insertIncentend(int patient_id, String details, int medication) {
 		// TODO Auto-generated method stub
 		try {
 			connectDB connection = new connectDB();
-			String query1 = "INSERT INTO incidents (Details, Patient_ID, Date, Medication_ID) VALUES ('"
-					+ details
-					+ "', "
-					+ patient_id
-					+ ", CURDATE() ,"
-					+ medication + ");";
+			String query1= "INSERT INTO incidents (Details, Patient_ID, Date, Medication_ID) VALUES ('"
+			+details+"', "+patient_id+", CURDATE() ,"+medication+");";
 			System.out.println(query1);
-			connection.stmt.executeUpdate(query1);
+			connection.stmt.executeUpdate(query1); 
 			return true;
-		} catch (SQLException se) {
-			// Handle errors for JDBC
-			se.printStackTrace();
-			return false;
+		}catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		      return false;
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
 			return false;
 		}
 	}
-
-	public String checkForAllergies(int p, int m) {
-		try {
+	
+	public 	String checkForAllergies(int p , int m){
+		try{
 			connectDB connection = new connectDB();
-			String query = "SELECT * FROM `allergies` WHERE allergies.Patient_ID = "
-					+ p + " and allergies.Medication_ID= " + m + " ;";
+			String query = "SELECT * FROM `allergies` WHERE allergies.Patient_ID = "+p+" and allergies.Medication_ID= "+m+" ;";
 			connection.resSet = connection.stmt.executeQuery(query);
 			System.out.println(query);
-
-			if (connection.resSet.next()) {
+			
+			if(connection.resSet.next()){
 				return connection.resSet.getString("Details");
 			}
-		} catch (SQLException se) {
-			se.printStackTrace();
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return "";
 	}
-
-	public String[] getDrugInfo(int dr_id) {
-		try {
+	
+	public String [] getDrugInfo (int dr_id){
+		try{
 			connectDB connection = new connectDB();
-			String query = "SELECT * FROM `drugs` WHERE drugs.Medication_ID = "
-					+ dr_id + ";";
+			String query = "SELECT * FROM `drugs` WHERE drugs.Medication_ID = "+dr_id+";";
 			connection.resSet = connection.stmt.executeQuery(query);
-			String[] a = new String[2];
-			if (connection.resSet.next()) {
-				a[0] = connection.resSet.getString("Medication_Name");
+			String [] a = new String[2];
+			if (connection.resSet.next()){
+				a[0]=connection.resSet.getString("Medication_Name");
 				a[1] = connection.resSet.getString("Discreption");
 			}
 			return a;
-		} catch (SQLException se) {
-			se.printStackTrace();
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return null;
 	}
-
-	public boolean updateDropIn(int randevou, int d_id) {
+	
+	public boolean updateDropIn(int randevou, int d_id){
 		try {
 			connectDB connection = new connectDB();
-			String query1 = "UPDATE randevou SET  drop_in = 1, Drop_in_date = NOW() ,Drop_in_doc = "
-					+ d_id + " WHERE Randevou_ID = " + randevou + ";";
-			System.out.println(query1);
-			connection.stmt.executeUpdate(query1);
-
+			String query1= "UPDATE randevou SET  drop_in = 1, Drop_in_date = NOW() ,Drop_in_doc = "+d_id+ " WHERE Randevou_ID = "+randevou+";";
+					System.out.println(query1);
+			connection.stmt.executeUpdate(query1); 
+			
 			return true;
-		} catch (SQLException se) {
-			// Handle errors for JDBC
-			se.printStackTrace();
+		}catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
+		};
 		return false;
 	}
-
-	public boolean SendEmailtoALL(int p_id, String remail, String Name) {
-		try {
+	
+	
+	
+	public boolean SendEmailtoALL(int p_id,String remail, String Name){
+		try{
 			connectDB connection = new connectDB();
-			String query = "SELECT Email FROM `staff` WHERE Doctor = 1 or Nurse=1 ";
+			String query = "SELECT Email FROM `staff` WHERE Doctor = 1 or Nurse=1 " ;
 			connection.resSet = connection.stmt.executeQuery(query);
-
-			while (connection.resSet.next()) {
-				send(connection.resSet.getString(1), p_id, Name);
+			
+			
+			while(connection.resSet.next()){
+				send(connection.resSet.getString(1),p_id,Name);
 			}
-			send(remail, p_id, Name);
+			send(remail,p_id,Name);
 			return true;
-		} catch (SQLException se) {
-			se.printStackTrace();
+		}catch(SQLException se){
+		      se.printStackTrace();
 		} catch (Exception ex) {
 			System.out.println("ERROR" + ex);
-		}
-		;
-
+		};
+		
 		return false;
 	}
-
-	static String d_email = "pparthenhs@gmail.com";// to email mas
-	static String d_password = "jgfaksjbfkas8934j@njms$";// o kwdikos mas
+	
+	static String d_email = "pparthenhs@gmail.com";//to email mas 
+	static String d_password = "jgfaksjbfkas8934j@njms$";// o kwdikos mas 
 	static String d_host = "smtp.gmail.com";// o host
 	static String d_port = "465";// to port
-	static String m_to = "kkoushi_antria13@hotmail.com";// to email pou phgenei
+	static String m_to = "kkoushi_antria13@hotmail.com";//to email pou phgenei
 	static String m_subject = "To email einai etoimo ";
 	static String m_text = "Hey, this is a test email.";
-
+    
 	static private class SMTPAuthenticator extends javax.mail.Authenticator {
-		public PasswordAuthentication getPasswordAuthentication() {
-			return new PasswordAuthentication(d_email, d_password);
-		}
-	}
-
+        public PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(d_email, d_password);
+        }
+    }
+    
 	/**
 	 * 
-	 * This method sent the e-mail to the customer and attached the proper pdf
-	 * file
+	 * This method sent the e-mail to the customer and 
+	 * attached the proper pdf file 
 	 *
-	 * @param mailtoSend
-	 *            - customer email
-	 * @param s5
-	 *            -id customer
-	 * @param s6
-	 *            -customer First name
-	 * @param s7
-	 *            - customer last name
-	 * @param s8
-	 *            -type of file (find the folder)
+	 *@param mailtoSend - customer email
+	 *@param s5-id customer 
+	 *@param s6-customer First name 
+	 *@param s7- customer last name
+	 *@param s8-type of file (find the folder)
 	 */
-	public static void send(String mailtoSend, int id, String name) {
+    public static void send(String mailtoSend,int id,String name){
+    
+    	//m_to=mailtoSend;
+    	m_subject="Self harm Pasient";
+    	m_text="The Pesient with name : "+name+"and with ID:"+id+"is a self harm person";
+        Properties props = new Properties();//ena hashtable
+        
+        props.put("mail.smtp.user", d_email);
+        props.put("mail.smtp.host", d_host);
+        props.put("mail.smtp.port", d_port);
+        
+        
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.socketFactory.port", d_port);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+        
+        props.setProperty("mail.user", d_email);
+        props.setProperty("mail.password", d_password);
+        
+        try {
+        	
+           Authenticator auth = new SMTPAuthenticator();
+           Session session = Session.getInstance(props,auth); 
+              
+            Message msg = new MimeMessage(session);
+             
+            	msg.setSubject(m_subject);
+            	msg.setFrom(new InternetAddress(d_email));
+            	msg.addRecipient(Message.RecipientType.TO, new InternetAddress(m_to));
+            
+//            BodyPart messageBodyPart = new MimeBodyPart();
+            
+            //	messageBodyPart.setText(m_text);
+            
+//            Multipart multipart = new MimeMultipart();
+//
+//            multipart.addBodyPart(messageBodyPart);
+//            
+//            messageBodyPart = new MimeBodyPart();
+//            
+//            String file = "C:/Users/Panos/Desktop/TEAM_D_361/"+s8+"/"+ s5 +"_" + s6 + "_"+ s7 + ".pdf";
+//            
+//            String fileName =  s5 +"_" + s6 + "_"+ s7 + ".pdf";
+//            
+//            DataSource source = new FileDataSource(file);
+//            
+//            messageBodyPart.setDataHandler(new DataHandler(source));
+//            messageBodyPart.setFileName(fileName);
+//            multipart.addBodyPart(messageBodyPart);
 
-		// m_to=mailtoSend;
-		m_subject = "Self harm Pasient";
-		m_text = "The Pesient with name : " + name + "and with ID:" + id
-				+ "is a self harm person";
-		Properties props = new Properties();// ena hashtable
-
-		props.put("mail.smtp.user", d_email);
-		props.put("mail.smtp.host", d_host);
-		props.put("mail.smtp.port", d_port);
-
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.socketFactory.port", d_port);
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.socketFactory.fallback", "false");
-
-		props.setProperty("mail.user", d_email);
-		props.setProperty("mail.password", d_password);
-
-		try {
-
-			Authenticator auth = new SMTPAuthenticator();
-			Session session = Session.getInstance(props, auth);
-
-			Message msg = new MimeMessage(session);
-
-			msg.setSubject(m_subject);
-			msg.setFrom(new InternetAddress(d_email));
-			msg.addRecipient(Message.RecipientType.TO,
-					new InternetAddress(m_to));
-
-			msg.setText(m_text);
-
-			Transport.send(msg);
-			System.out.print("edw");
-
-		} catch (Exception mex) {
-			mex.printStackTrace();
-		}
-	}
+            msg.setText(m_text);
+            
+           // msg.setContent(multipart);
+          
+          
+            Transport.send(msg);
+            System.out.print("edw");
+   
+        } catch (Exception mex) {
+            mex.printStackTrace();
+        }
+    }
+   
 
 }
